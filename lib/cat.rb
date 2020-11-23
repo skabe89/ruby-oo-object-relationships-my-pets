62e282fe4e00ea139b5a1935a34e0561b647f35f
Sean <skabe89@gmail.com>
2020-11-23 17:31:08 -0500

    commit tree 36906f8f2636318d08746a30927ff023bb78eb05

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all_cats = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all_cats << self
+  end
+
+  def self.all
+    @@all_cats
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
 class Owner
-  # code goes here
+  attr_accessor 
+  attr_reader :name, :species
+  @@all_owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_owners << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.count
+    @@all_owners.length
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.reset_all
+    @@all_owners.clear
+  end
+
+  def cats
+    Cat.all.select{|i| i.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|i| i.owner == self}
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+  end
+
+  def walk_dogs
+    dogs.each{|i| i.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.each{|i| i.mood = "happy"}
+  end
+
+  def sell_pets
+    dogs.each{|i| i.mood = "nervous"}
+    dogs.each{|i| i.owner = nil}
+    cats.each{|i| i.mood = "nervous"}
+    cats.each{|i| i.owner = nil}
+  end
+
+  def list_pets
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
 end
